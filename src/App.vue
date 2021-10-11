6fd3b91515a1f47f20b924112d5aaf2545da9d17
teerryn <slaughterer90@gmail.com>
2021-10-11 18:35:29 +0100

    first commit

---
+++ src/App.vue
@@ -0,0 +1,14 @@
+<template>
+  <Form @submit="handleSubmit">
+    <Field as="input" label="Name" name="firstname" />
+    <button type="submit">Submit</button>
+  </Form>
+</template>
+
+<script setup lang="ts">
+import { Form, Field } from 'vee-validate';
+
+const handleSubmit = (data: { name: string }) => {
+  console.log(data);
+};
+</script>
